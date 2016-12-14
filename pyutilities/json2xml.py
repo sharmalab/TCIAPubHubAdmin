e49633eba2fc17f6db3967d920577d50a3336818
Deh Jun Tzou <junior.tzou@gmail.com>
2016-12-14 15:35:48 -0500

    add xml schema validation; add datacite .xsd files

--- pyutilities/json2xml.py
+++ pyutilities/json2xml.py
@@ -1,11 +1,32 @@
-import json, sys, xml.dom.minidom
+import json, sys, xml.dom.minidom, urllib
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element, tostring
 from collections import OrderedDict
+from lxml import etree
+
+
+def validate(xml_doc, xml_schema_url):
+    """Validate XML document against XML schema given by url
+    :type xml_doc: str
+    :type xml_schema_url: str
+    """
+
+    u = urllib.urlopen(xml_schema_url)
+    xml_schema_data = u.read()
+    xml_schema_doc = etree.XML(xml_schema_data)
+    xml_schema = etree.XMLSchema(xml_schema_doc)
+    xml_doc = etree.XML(xml_doc)
+    try:
+        xml_schema.assertValid(xml_doc)    # If validation fails, exception is raised
+    except Exception, e:
+        print("Invalid")
+        raise e
+    else:
+        pass
 
 
 def build(root, obj):
-    """Build an XML string from a Python dictionary recursively
+    """Build the XML document tree from a Python dictionary recursively
     :type root: Element
     :type obj: dict
     """
@@ -18,7 +39,7 @@ def build(root, obj):
                   root.set(attr_k, attr_v)    # if key is "ATTR", set attributes for the current element/tag
             else:
                 if k == "VAL":
-                    build(root, v)     # If key is "VAL", then the current element's text will be the value
+                    build(root, v)    # If key is "VAL", then the current element's text will be the value
                 else:
                     child = Element(k)
                     build(child, v)
@@ -75,13 +96,13 @@ def convert():
       },
       "relatedIdentifiers": [
         {
-          "relatedIdentifer": {
+          "relatedIdentifier": {
             "VAL": "24560287",
             "ATTR": {"relationType": "IsSupplementTo", "relatedIdentifierType": "PMID"}
           }
         },
         {
-          "relatedIdentifer": {
+          "relatedIdentifier": {
             "VAL": "10.1016/j.mri.2014.01.004",
             "ATTR": {"relationType": "IsSupplementTo", "relatedIdentifierType": "DOI"}
           }
@@ -98,50 +119,19 @@ def convert():
     }
     """
 
-    """Sample XML output:
-    <?xml version="1.0"?>
-    <resource xmlns="http://datacite.org/schema/kernel-3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://datacite.org/schema/kernel-3 http://schema.datacite.org/meta/kernel-3/metadata.xsd">
-      <identifier identifierType="DOI">10.7937/K9/TCIA.2016.FADS26KG</identifier>
-      <creators>
-        <creator>
-          <creatorName>Fedorov, Andriy</creatorName>
-        </creator>
-        <creator>
-          <creatorName>Tempany, Clare</creatorName>
-        </creator>
-      </creators>
-      <titles>
-        <title>Data From QIN PROSTATE</title>
-      </titles>
-      <publisher>The Cancer Imaging Archive</publisher>
-      <publicationYear>2016</publicationYear>
-      <contributors>
-        <contributor contributorType="DataCurator">
-          <contributorName>TCIA Team</contributorName>
-          <affiliation>The Cancer Imaging Archive</affiliation>
-        </contributor>
-      </contributors>
-      <resourceType resourceTypeGeneral="Dataset">DICOM</resourceType>
-      <relatedIdentifiers>
-        <relatedIdentifer relatedIdentifierType="PMID" relationType="IsSupplementTo">24560287</relatedIdentifer>
-        <relatedIdentifer relatedIdentifierType="DOI" relationType="IsSupplementTo">10.1016/j.mri.2014.01.004</relatedIdentifer>
-      </relatedIdentifiers>
-      <descriptions>
-        <description descriptionType="Abstract">This collection contains multiparametric MRI images collected for the purposes of detection...</description>
-      </descriptions>
-    </resource>
-    """
-
-    # obj = json.loads(sample_string, object_pairs_hook=OrderedDict) # Use sample_string as input
-
-    obj = json.load(sys.stdin, object_pairs_hook=OrderedDict)     # Read JSON data from standard input into dictionary
-    root = Element("resource", attrib={"xmlns":"http://datacite.org/schema/kernel-3", "xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance", "xsi:schemaLocation":"http://datacite.org/schema/kernel-3 http://schema.datacite.org/meta/kernel-3/metadata.xsd"})     # Create XML root element. This is constant.
-    build(root, obj)     # Build XML string from dictionary
-    parsed = xml.dom.minidom.parseString(ElementTree.tostring(root))
-    print parsed.toprettyxml()     # Write and format XML string to standard output
+    #obj = json.loads(sample_string, object_pairs_hook=OrderedDict)    # Use sample_string as input
+    obj = json.load(sys.stdin, object_pairs_hook=OrderedDict)    # Read JSON data from standard input into dictionary
+    root = Element("resource", attrib={"xmlns":"http://datacite.org/schema/kernel-3",
+                                       "xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance",
+                                       "xsi:schemaLocation":"http://datacite.org/schema/kernel-3" + " " +
+                                       "http://schema.datacite.org/meta/kernel-3/metadata.xsd"})    # Create XML root element. This is constant.
+    build(root, obj)    # Build XML document tree from dictionary
+    xml_doc = ElementTree.tostring(root)
+    ignore, xml_schema_url = root.attrib.get("xsi:schemaLocation").split()
+    validate(xml_doc, xml_schema_url)    # Validate XML document against XML schema
+    parsed = xml.dom.minidom.parseString(xml_doc)
+    print parsed.toprettyxml()    # Pretty print XML document to standard output
 
 
 if __name__ == '__main__':
     convert()
-
-
