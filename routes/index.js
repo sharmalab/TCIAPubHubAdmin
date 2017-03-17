722932880854b16447589072b39e6b68cb08ec8f
Ganesh Iyer <lastlegion@gmail.com>
2017-03-17 15:36:14 -0400

    fixed newlines and percentages in description parse error

--- routes/index.js
+++ routes/index.js
@@ -233,7 +233,7 @@ function createJSON(formdata) {
       "descriptions": [
         {
           "description": {
-            "VAL": formdata.description,
+            "VAL": cleanData(formdata.description),
             "ATTR": {"descriptionType": "Abstract"}
           }
         }
@@ -272,6 +272,12 @@ var checkRequiredFields = function(form_data){
 
 
 winston.log("hello");
+var cleanData = function(data){
+  var data = data;
+  data = data.replace(/(\r\n|\n|\r|\%)/gm,"");
+  return data;
+  //return escape(data);
+}
 
 router.post("/api/createDOI", function(req, res) {
     winston.log("POST")
@@ -280,6 +286,8 @@ router.post("/api/createDOI", function(req, res) {
     console.log("submit"); 
     console.log(JSON.stringify(req.body));
     var form_data = req.body.formData;
+
+    //form_data = cleanData(form_data);
     var mongo_data = JSON.parse(JSON.stringify(form_data));
     console.log(form_data);
 
@@ -300,7 +308,7 @@ router.post("/api/createDOI", function(req, res) {
 
     form_data.url = URL;
     form_data.doi = DOI;
-   
+     
     resources.doi = form_data.doi;
     if(!checkRequiredFields(form_data)){
       return res.status(400).send("Missing required fields in the form");
