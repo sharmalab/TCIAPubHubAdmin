340bf7fa6aa068cd96b8fcc879444ad7b93c514f
Ryan Birmingham <birm@rbirm.us>
2017-05-22 15:53:39 -0400

    add jnlp standalone and tester

---
+++ jnlptest.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+
+
+app.post("/api/createJNLP", function(req, res) {
+  return res.json({
+    jnlp: "/JNLP/hi"
+  });
+});
+
+app.get("/api/createJNLP", function(req, res) {
+  res.send("hi");
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+})
