0c999da7cdb13672f08755592bea8b1028e00aac
Ryan Birmingham <birm@rbirm.us>
2017-07-25 16:46:58 -0400

    workarounds for citations

--- ezidToMongo.py
+++ ezidToMongo.py
@@ -145,7 +145,7 @@ for target_url in urls:
                 "url":doiurl,
                 "description":abstract,
                 "year":pubyr,
-                "doi":doiurl,
+                "doi":"123456789012345678"+doiid,
                 "publisher":publisher
                 }
         collection.insert_one(post).inserted_id
