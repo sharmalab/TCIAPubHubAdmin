5f17faa018de68b37b97eed50ab3d2f1347e9397
Ryan Birmingham <birm@rbirm.us>
2018-08-28 13:06:35 -0400

    add failing dummy test

--- tests/routeTest.js
+++ tests/routeTest.js
@@ -4,4 +4,7 @@ describe("App API Route Tests", function(){
   it("should work as testing framework", async function(){
     expect(false).to.be.false
   })
+  it("should error here", async function(){
+    expect(true).to.be.false
+  })
 })
